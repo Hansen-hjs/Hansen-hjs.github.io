9989a64c7f47904a86ae21e581df951e85090d74
黄景圣 <1141752341@qq.com>
2017-10-09 08:47:42 +0800

    个人简历

--- myCV/js/index.js
+++ myCV/js/index.js
@@ -1,20 +1,6 @@
-// (function(){
-//   var userAgent = window.navigator.userAgent.toLowerCase();
-//   $.browser.msie10 = $.browser.msie && /msie 10\.0/i.test(userAgent);
-//   $.browser.msie9 = $.browser.msie && /msie 9\.0/i.test(userAgent);
-//   $.browser.msie8 = $.browser.msie && /msie 8\.0/i.test(userAgent);
-//   $.browser.msie7 = $.browser.msie && /msie 7\.0/i.test(userAgent);
-//   $.browser.msie6 = !$.browser.msie8 && !$.browser.msie7 && $.browser.msie && /msie 6\.0/i.test(userAgent);
-//   if($.browser.msie6){
-//      window.location.href="http://modesign.cc/outdatedbrowser/";
-//   }if($.browser.msie7){
-//      window.location.href="http://modesign.cc/outdatedbrowser/";
-//   }if($.browser.msie8){
-//      window.location.href="http://modesign.cc/outdatedbrowser/";
-//   }if($.browser.msie9){
-//      window.location.href="http://modesign.cc/outdatedbrowser/";
-//   }
-// })();
+// if (ieVer <= 9) {
+//   window.location.href="http://modesign.cc/outdatedbrowser/";
+// }
 var box = document.getElementById('wrap');
 window.onload = function () {
   box.querySelector('.loding').classList.add('loding_hide');
