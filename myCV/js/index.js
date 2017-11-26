19b22c2f54a9495c601135f2568c910d1faf2ac9
黄景圣 <1141752341@qq.com>
2017-11-26 15:59:25 +0800

    个人简历

--- myCV/js/index.js
+++ myCV/js/index.js
@@ -1,28 +1,35 @@
 // if (ieVer <= 9) {
 //   window.location.href="http://modesign.cc/outdatedbrowser/";
 // }
-var box = document.getElementById('wrap');
-window.onload = function () {
+const box = document.getElementById('wrap');
+window.addEventListener('DOMContentLoaded', () => {
   box.querySelector('.loding').classList.add('loding_hide');
-  setTimeout(function(){
+  setTimeout(() => {
     box.removeChild(box.querySelector('.loding'));
     document.body.style.overflowY = 'auto';
   },500);
-  var leftH2 = box.querySelector('.left_top');
-  var rightH2 = box.querySelector('.right_bottom');
-  var textLi = box.querySelectorAll('.text_from li');
-  setTimeout(function () {
+  let leftH2 = box.querySelector('.left_top');
+  let rightH2 = box.querySelector('.right_bottom');
+  let textLi = box.querySelectorAll('.text_from li');
+  setTimeout(() => {
     leftH2.classList.remove('inleft');
     rightH2.classList.remove('inright');
-    setTimeout(function () {
-      for (var i = 0; i < textLi.length; i++) {
-        (function (j) {
-          setTimeout(function () {
-            textLi[j].classList.add('li_translate');
-          }, 200 + 200 * j);
-        })(i)
+    setTimeout(() => {
+      for (let i = 0; i < textLi.length; i++) {
+        setTimeout(function () {
+          textLi[i].classList.add('li_translate');
+        },200+200 * i);
       }
     }, 500);
+    // setTimeout(function() {
+    //   for (var i = 0; i < textLi.length; i++) {
+    //     (function (j) {
+    //       setTimeout(function () {
+    //         textLi[j].classList.add('li_translate');
+    //       }, 200 + 200 * j);
+    //     })(i)
+    //   }
+    // }, 500);
   }, 500);
   // swiper
   var mySwiper = new Swiper ('#akk', {
@@ -57,37 +64,40 @@ window.onload = function () {
     lazyLoadingInPrevNext : true,
     loop: true
   });
-};
+});
 // 窗口变动改变尺寸
 // window.onresize = function(){
 //
 // }
 
-function goTop(clickEleSelector){
+var goTop = (clickEleSelector) => {
 	var clickEle = document.querySelector(clickEleSelector);
 	var intervalId = null;
   var isExecuted = true;  // 重复点击
   var rootNode = document.body.scrollTop === 0 ? document.documentElement : document.body;
-  window.addEventListener('scroll', function(){
-		if(rootNode.scrollTop > (document.body.scrollHeight/2)){
+  window.addEventListener('scroll', () => {
+		if(rootNode.scrollTop > (document.body.scrollHeight / 2)){
 			clickEle.style.visibility = 'visible';
 		}else{
 			clickEle.style.visibility = 'hidden';
 		}
 	});
-	clickEle.addEventListener('click', function () {
+  // 滚轮事件
+	document.body.addEventListener('mousewheel', () => {
+		clearInterval(intervalId);
+	});
+  // 点击事件
+	clickEle.addEventListener('click', () => {
     if (isExecuted) {
       isExecuted = false;
-      intervalId = setInterval(function () {
+      intervalId = setInterval(() => {
         rootNode.scrollTop = rootNode.scrollTop - rootNode.scrollTop * 0.1;
         if(rootNode.scrollTop <= 0){
           clearInterval(intervalId)
         }
       }, 10);
     }
-    setTimeout(function () {
-       isExecuted = true;
-    },500);
+    setTimeout(() => isExecuted = true ,500);
 	});
 }
-goTop('#gotop')
+goTop('#gotop');
