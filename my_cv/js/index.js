dfd5c2dd2f89d3687a941bfb5ae2c3edd6d39762
Hansen <1141752341@qq.com>
2018-08-07 11:59:04 +0800

    个人简历

---
+++ my_cv/js/index.js
@@ -0,0 +1,17 @@
+var ver = (function () {
+    var rv = -1;
+    if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
+        if (re.exec(ua) != null)
+            rv = parseFloat(RegExp.$1);
+    }
+    return rv;
+})();
+if (ver > -1) {
+    if (ver < 10) {
+        return window.location.href = 'http://modesign.cc/outdatedbrowser/';
+    } else {
+        console.log('当前IE浏览版本为：', ver);
+    }
+}
